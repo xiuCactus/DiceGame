52d42782c4d23ecc2c950d8ede166d593acd5746
ReamLab <ream712@gmail.com>
2025-01-07 20:50:32 +0100

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNumber;
+            int enemyRandomNumber;
+            int playerpoints = 0;
+            int AIpoints = 0;
+            
+            Random random = new Random();
+            
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("press any key to roll the dice");
+
+                Console.ReadKey();
+
+                playerRandomNumber = random.Next(1, 7);
+                
+                Console.WriteLine("your number is: " + playerRandomNumber);
+
+                Console.WriteLine("waiting ... ");
+                System.Threading.Thread.Sleep(1000);
+
+                enemyRandomNumber = random.Next(1, 7);
+                Console.WriteLine("AI number is: "+ enemyRandomNumber);
+
+                if (playerRandomNumber>enemyRandomNumber)
+                {
+                    playerpoints++;
+                    Console.WriteLine("palyer Wins this round");
+                }
+                else if (playerRandomNumber<enemyRandomNumber)
+                {
+                    AIpoints++;
+                    Console.WriteLine("AI wins this round ");
+                }
+                else
+                {
+                    Console.WriteLine("Tie");
+                }
+
+                Console.WriteLine("player: "+ playerpoints + "  AI: "+AIpoints + ".");
+                Console.WriteLine();
+            }
+            
+            if (playerpoints>AIpoints)
+            {
+                Console.WriteLine("You Wins!");
+            }
+            else if (playerpoints<AIpoints)
+            {
+                Console.WriteLine("You lose!");
+            }
+            else
+            {
+                Console.WriteLine("it's a Tie");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+ 
